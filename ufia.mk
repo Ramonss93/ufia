a8b2b82a2e3d018a598a9d39a03cafdb1f29e398
TedwardErker <tedward.erker@gmail.com>
2015-07-27 13:06:34 -0500

    kang edits for texture

--- ufia.mk
+++ ufia.mk
@@ -1,11 +1,11 @@
 
 # Add Ratios Small Image
-PAN_SPOT/SPOT_PanSharp_subsubset_wRatios.tif: R/addRatiosImage.R
-	cd R; Rscript addRatiosImage.R SPOT_PanSharp_subsubset PAN_SPOT
+#PAN_SPOT/SPOT_PanSharp_subsubset_wRatios.tif: R/addRatiosImage.R
+#	cd R; Rscript addRatiosImage.R SPOT_PanSharp_subsubset PAN_SPOT
 
 # Add Ratios Large Image
-PAN_SPOT/geomatica_SPOT_panshp_wRatios.tif: R/addRatiosImage.R
-	cd R; Rscript addRatiosImage.R geomatica_SPOT_panshp PAN_SPOT
+#PAN_SPOT/geomatica_SPOT_panshp_wRatios.tif: R/addRatiosImage.R
+#	cd R; Rscript addRatiosImage.R geomatica_SPOT_panshp PAN_SPOT
 
 # Convert to dataframe
 #PAN_SPOT/geomatica_SPOT_panshp_wAddedFeatures_df.Rdata: R/convertImageToDF.R
@@ -13,10 +13,10 @@ PAN_SPOT/geomatica_SPOT_panshp_wRatios.tif: R/addRatiosImage.R
 
 
 # Add texture to Small Image, 5 is the NDVI layer
-PAN_SPOT/SPOT_PanSharp_subsubset_wTexture.tif: R/addTextureImage.R PAN_SPOT/SPOT_PanSharp_subsubset_wRatios.tif
-	cd R; Rscript addTextureImage.R PAN_SPOT SPOT_PanSharp_subsubset_wRatios 5
+#PAN_SPOT/SPOT_PanSharp_subsubset_wRatios_wTexture.tif: R/addTextureImage.R PAN_SPOT/SPOT_PanSharp_subsubset_wRatios.tif
+#	cd R; Rscript addTextureImage.R PAN_SPOT SPOT_PanSharp_subsubset_wRatios 5
 
 # Add texture to Large Image, 5 is the NDVI layer
-PAN_SPOT/geomatica_SPOT_panshp_wTexture.tif: R/addTextureImage.R PAN_SPOT/geomatica_SPOT_panshp_wRatios.tif
+PAN_SPOT/geomatica_SPOT_panshp_wRatios_wTexture.tif: R/addTextureImage.R PAN_SPOT/geomatica_SPOT_panshp_wRatios.tif
 	cd R; Rscript addTextureImage.R PAN_SPOT geomatica_SPOT_panshp_wRatios 5
 
