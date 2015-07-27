2c28f6bd1a632b585c44813f35332761151d0258
TedwardErker <tedward.erker@gmail.com>
2015-07-27 08:01:00 -0500

    small modifications from kang

--- R/addLayersImage.R
+++ R/addLayersImage.R
@@ -1,6 +1,3 @@
-list.of.packages <- c("glcm","raster","spatial.tools","dplyr","doParallel")
-new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
 
 library(glcm)
 library(raster)
--- ufia.mk
+++ ufia.mk
@@ -1,8 +1,8 @@
 
 
 # Testing on small image
-PAN_SPOT/SPOT_PanSharp_subsubset_wAddedFeatures.tif: R/addLayersImage.R
-	cd R; Rscript addLayersImage.R SPOT_PanSharp_subsubset PAN_SPOT
+#PAN_SPOT/SPOT_PanSharp_subsubset_wAddedFeatures.tif: R/addLayersImage.R
+#	cd R; Rscript addLayersImage.R SPOT_PanSharp_subsubset PAN_SPOT
 
 # Large Image
 PAN_SPOT/geomatica_SPOT_panshp_wAddedFeatures.tif: R/addLayersImage.R
